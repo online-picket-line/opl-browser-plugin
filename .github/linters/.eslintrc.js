b89358133c171569c2d9714e2fc50b8732d541ad
OPL <oplfun@onlinepicketline.com>
2025-12-30 11:48:10 -0700

    chore: update ESLint configuration to refine globals and rules for better linting in tests

--- .github/linters/.eslintrc.js
+++ .github/linters/.eslintrc.js
@@ -10,7 +10,6 @@ module.exports = [
       'coverage/**',
       'generate-icons.js',
       'test-import.js',
-      'tests/**',
       '*.min.js'
     ],
     languageOptions: {
@@ -26,14 +25,82 @@ module.exports = [
         console: 'readonly',
         document: 'readonly',
         window: 'readonly',
-        fetch: 'readonly'
+        fetch: 'readonly',
+        URL: 'readonly',
+        btoa: 'readonly',
+        atob: 'readonly',
+        performance: 'readonly',
+        setTimeout: 'readonly',
+        Date: 'readonly',
+        Math: 'readonly',
+        String: 'readonly',
+        Object: 'readonly',
+        Array: 'readonly',
+        Promise: 'readonly',
+        Error: 'readonly',
+        sessionStorage: 'readonly',
+        history: 'readonly',
+        require: 'readonly',
+        process: 'readonly',
+        __dirname: 'readonly'
       }
     },
     rules: {
-      'no-unused-vars': 'off',
+      'no-console': 'off',
+      'no-debugger': 'warn',
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_|^e$|^e2$|^data$|^error$|^response$|^result$|^timestamp$|^changes$|^actionType$|^urlToTest$|^actions$|^blockMode$|^manyActions$|^testButton$' }],
+      'no-undef': 'error',
+      'no-useless-escape': 'warn'
+    }
+  },
+  {
+    files: ['tests/**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'script',
+      globals: {
+        chrome: 'readonly',
+        browser: 'readonly',
+        importScripts: 'readonly',
+        ApiService: 'readonly',
+        module: 'readonly',
+        exports: 'readonly',
+        console: 'readonly',
+        document: 'readonly',
+        window: 'readonly',
+        fetch: 'readonly',
+        URL: 'readonly',
+        btoa: 'readonly',
+        atob: 'readonly',
+        performance: 'readonly',
+        setTimeout: 'readonly',
+        Date: 'readonly',
+        Math: 'readonly',
+        String: 'readonly',
+        Object: 'readonly',
+        Array: 'readonly',
+        Promise: 'readonly',
+        Error: 'readonly',
+        sessionStorage: 'readonly',
+        history: 'readonly',
+        require: 'readonly',
+        process: 'readonly',
+        __dirname: 'readonly',
+        describe: 'readonly',
+        it: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+        expect: 'readonly',
+        jest: 'readonly',
+        matchUrlToAction: 'readonly'
+      }
+    },
+    rules: {
       'no-console': 'off',
       'no-debugger': 'off',
-      'no-undef': 'error'
+      'no-unused-vars': 'off',
+      'no-undef': 'warn',
+      'no-useless-escape': 'warn'
     }
   }
 ];
