a17c65a638ff3740254f6690d28167d9f47a8cfc
OPL <oplfun@onlinepicketline.com>
2025-12-30 12:18:39 -0700

    chore: remove old ESLint configuration and migrate to new eslint.config.mjs format

---
+++ .github/linters/eslint.config.mjs
@@ -0,0 +1,55 @@
+export default [
+  {
+    ignores: [
+      'node_modules/**',
+      'browser-polyfill.js',
+      'coverage/**',
+      'generate-icons.js',
+      'test-import.js',
+      'eslint.config.js',
+      'tests/**'
+    ]
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'script',
+      globals: {
+        chrome: 'readonly',
+        browser: 'readonly',
+        console: 'readonly',
+        document: 'readonly',
+        window: 'readonly',
+        fetch: 'readonly',
+        URL: 'readonly',
+        btoa: 'readonly',
+        atob: 'readonly',
+        performance: 'readonly',
+        setTimeout: 'readonly',
+        Date: 'readonly',
+        Math: 'readonly',
+        String: 'readonly',
+        Object: 'readonly',
+        Array: 'readonly',
+        Promise: 'readonly',
+        Error: 'readonly',
+        importScripts: 'readonly',
+        ApiService: 'readonly',
+        sessionStorage: 'readonly',
+        history: 'readonly',
+        module: 'readonly',
+        exports: 'readonly',
+        require: 'readonly',
+        process: 'readonly',
+        __dirname: 'readonly'
+      }
+    },
+    rules: {
+      'no-console': 'off',
+      'no-debugger': 'off',
+      'no-unused-vars': 'off',
+      'no-undef': 'off'
+    }
+  }
+];
